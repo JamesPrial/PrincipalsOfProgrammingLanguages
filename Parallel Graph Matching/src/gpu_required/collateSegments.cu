5e416bd72d1f9f1db70830be0d282ecbbdf93cc1
JamesPrial <prialjames17@gmail.com>
2024-08-09 19:57:27 -0400

    MODIFIED BY JAMES PRIAL

--- Parallel Graph Matching/src/gpu_required/collateSegments.cu
+++ Parallel Graph Matching/src/gpu_required/collateSegments.cu
@@ -8,5 +8,34 @@
 #include <stdlib.h>
 
 __global__ void collateSegments_gpu(int * src, int * scanResult, int * output, int numEdges) {
-	/*YOUR CODE HERE*/
+	unsigned int tid = blockIdx.x * blockDim.x + threadIdx.x;
+	unsigned int totalThreads = blockDim.x * gridDim.x;
+	if(totalThreads >= numEdges){
+		if(tid >= numEdges){
+			return;
+		}
+		if(tid == (numEdges - 1)){
+			output[src[tid]] = scanResult[tid];
+			return;
+		}
+		if(src[tid] == src[tid + 1]){
+			return;
+		}
+		output[src[tid]] = scanResult[tid];
+	} else{
+		int tasks = (numEdges / totalThreads) + 1;
+		int i;
+		for(i = 0; i < tasks; i++){
+			unsigned int taskId = tid + (i * totalThreads);
+			if(taskId >= numEdges){
+				return;
+			}
+			if(taskId == (numEdges - 1)){
+				output[src[taskId]] = scanResult[taskId];
+			}else if(src[taskId] != src[taskId + 1]){
+				output[src[taskId]] = scanResult[taskId];
+			} 
+			
+		}
+	}
 }
